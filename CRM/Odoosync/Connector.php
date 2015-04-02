78dbfb62167a521b35fdf791d2692c99756de909
Jaap Jansma <jaap@edeveloper.nl>
2015-04-02 13:29:37 +0200

    update

--- CRM/Odoosync/Connector.php
+++ CRM/Odoosync/Connector.php
@@ -74,7 +74,7 @@ class CRM_Odoosync_Connector {
       }
     }
 
-    $this->log('Could not login into Odoo');
+    $this->log('Could not login into Odoo (raw response: '.$resp->raw_data.' )');
     return false;
   }
   
